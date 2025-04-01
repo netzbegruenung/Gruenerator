e1d0fe50c6b9178fead0135c7ad0b526ff1ead07
Movm <moritzius007@hotmail.de>
2025-04-01 18:13:44 +0200

    updated quill

--- gruenerator_frontend/src/components/common/editor/hooks/index.js
+++ gruenerator_frontend/src/components/common/editor/hooks/index.js
@@ -1,5 +1,4 @@
 export { default as useTextHighlighting } from './useTextHighlighting';
 export { default as useNavigationWarning } from './useNavigationWarning';
-export { default as useQuillEditor } from './useQuillEditor';
 export { default as useProtectedHeaders } from './useProtectedHeaders';
 export { useClaudeResponse } from './useClaudeResponse'; 
