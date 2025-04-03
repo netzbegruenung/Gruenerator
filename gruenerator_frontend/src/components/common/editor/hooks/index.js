462d5f661121bca074a8868b41717e9043c683dd
Movm <moritzius007@hotmail.de>
2025-04-03 16:32:17 +0200

    Fixed editor and added html styling

--- gruenerator_frontend/src/components/common/editor/hooks/index.js
+++ gruenerator_frontend/src/components/common/editor/hooks/index.js
@@ -1,4 +1,3 @@
 export { default as useTextHighlighting } from './useTextHighlighting';
 export { default as useNavigationWarning } from './useNavigationWarning';
-export { default as useProtectedHeaders } from './useProtectedHeaders';
 export { useClaudeResponse } from './useClaudeResponse'; 
