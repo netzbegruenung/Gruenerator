7c8fff2aa089ac61f6565bd82a7554584daa0ef7
Movm <moritzius007@hotmail.de>
2025-05-08 00:05:59 +0200

    modified package & vite config

--- gruenerator_frontend/src/features/campaigns/components/CampaignDashboard.jsx
+++ gruenerator_frontend/src/features/campaigns/components/CampaignDashboard.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import templateData from '../../templates/canva/utils/templates.json';
 import FilesSection from './FilesSection';
 import TextsSection from './TextsSection';
 import TemplatesSection from './TemplatesSection';
