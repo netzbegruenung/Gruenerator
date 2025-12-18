101d219ad90e11c3b61f19bcc1361a4b57a316b1
movm <moritzius007@hotmail.de>
2025-12-18 02:11:10 +0100

    chore: Update CHANGELOG and add Changelog component

--- gruenerator_frontend/src/components/layout/Footer/Footer.jsx
+++ gruenerator_frontend/src/components/layout/Footer/Footer.jsx
@@ -53,6 +53,7 @@ const Footer = () => {
                                     <li><Link to="/support">Support</Link></li>
                                     <li><Link to="/impressum">Impressum</Link></li>
                                     <li><Link to="/datenschutz">Datenschutz</Link></li>
+                                    <li><Link to="/changelog">Changelog</Link></li>
                                     <li><a href="https://896ca129.sibforms.com/serve/MUIFAFnH3lov98jrw3d75u_DFByChA39XRS6JkBKqjTsN9gx0MxCvDn1FMnkvHLgzxEh1JBcEOiyHEkyzRC-XUO2DffKsVccZ4r7CCaYiugoiLf1a-yoTxDwoctxuzCsmDuodwrVwEwnofr7K42jQc-saIKeVuB_8UxrwS18QIaahZml1qMExNno2sEC7HyMy9Nz4f2f8-UJ4QmW" target="_blank" rel="noopener noreferrer">Newsletter</a></li>
                                 </ul>
                             </section>
