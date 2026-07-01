3ef41306aba6f5cd856b000898c7eeeeeeca34a2
Moritz Wächter <moritzwachter@MacBook-Air-von-Moritz.local>
2026-07-02 01:33:24 +0200

    refactor(web): remove dead utils, types, constants (knip)

--- apps/web/src/types/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './api';
-export * from './auth';
-export * from './common';
-export * from './documents';
-export * from './generator';
-export * from './baseform';
-export * from './notebook';
