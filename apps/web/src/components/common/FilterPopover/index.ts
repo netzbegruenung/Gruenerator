612a8906137594cdfe6e20fec7cc25fcf78c1995
Moritz Wächter <moritzwachter@MacBook-Air-von-Moritz.local>
2026-07-02 01:33:23 +0200

    refactor(components): remove 108 dead component files (knip)

--- apps/web/src/components/common/FilterPopover/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from './FilterPopover';
-export { default as FilterPopover } from './FilterPopover';
--- apps/web/src/components/common/Form/TypeformWizard/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as TypeformWizard } from './TypeformWizard';
